5e0090c23e35ad3c7a74b228d266b65dbaeb99f0
nadiiabilockon <11071995nad@gmail.com>
2020-04-30 22:15:58 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Container
+} from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div>
+      <Container>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
